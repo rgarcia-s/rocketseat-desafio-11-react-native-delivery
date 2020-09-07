8a7e199268ee20bfacffeb3baa39ee5f7eec1400
Raul Garcia <raul.gsouza@gmail.com>
2020-09-07 02:25:06 -0300

    Challenge completed

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,14 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      const response = await api.get('orders');
+
+      const formattedOrder: Food[] = response.data.map((order: Food) => ({
+        ...order,
+        formattedPrice: formatValue(order.price),
+      }));
+
+      setOrders(formattedOrder);
     }
 
     loadOrders();
